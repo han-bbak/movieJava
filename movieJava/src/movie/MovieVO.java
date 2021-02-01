d3822607d0f5057031f140e22995184c2eb08d27
user <user@DESKTOP-QR7ALM2>
2021-02-01 14:38:22 +0900

    MovieDAO, VO 수정

--- movieJava/src/movie/MovieVO.java
+++ movieJava/src/movie/MovieVO.java
@@ -1,131 +1,133 @@
 package movie;
 
 public class MovieVO {
-	private String M_CODE;
-	private String M_TITLE;
-	private String M_GENRE;
-	private String M_DIRECTOR;
-	private String M_DATE;
-	private String M_COUNTRY;
-	private String M_IMAGE;
-	private String M_SUMMARY;
-	private String M_RATING;
-	private int M_GRADE;
-	private String STATUS;
-
+	private String m_code;			// 영화코드
+	private String m_title;			// 영화제목
+	private String m_genre;			// 장르
+	private String m_director;		// 감독
+	private String m_date;			// 개봉연도
+	private String m_country;		// 국가
+	private String m_image;			// 이미지
+	private String m_summary;		// 설명
+	private String m_rating;		// 관람등급
+	private int m_grade;			// 평점
+	private String status;			// 게시여부
+	
 	public MovieVO() {
 	}
 
-	public MovieVO (String m_CODE, String m_TITLE, String m_GENRE, String m_DIRECTOR, String m_DATE, String m_COUNTRY,
-			String m_IMAGE, String m_SUMMARY, String m_RATING, int m_GRADE, String sTATUS) {
+	public MovieVO(String m_code, String m_title, String m_genre, String m_director, String m_date, String m_country,
+			String m_image, String m_summary, String m_rating, int m_grade, String status) {
 		super();
-		M_CODE = m_CODE;
-		M_TITLE = m_TITLE;
-		M_GENRE = m_GENRE;
-		M_DIRECTOR = m_DIRECTOR;
-		M_DATE = m_DATE;
-		M_COUNTRY = m_COUNTRY;
-		M_IMAGE = m_IMAGE;
-		M_SUMMARY = m_SUMMARY;
-		M_RATING = m_RATING;
-		M_GRADE = m_GRADE;
-		STATUS = sTATUS;
+		this.m_code = m_code;
+		this.m_title = m_title;
+		this.m_genre = m_genre;
+		this.m_director = m_director;
+		this.m_date = m_date;
+		this.m_country = m_country;
+		this.m_image = m_image;
+		this.m_summary = m_summary;
+		this.m_rating = m_rating;
+		this.m_grade = m_grade;
+		this.status = status;
 	}
 
-	public String getM_CODE() {
-		return M_CODE;
+	public String getM_code() {
+		return m_code;
 	}
 
-	public void setM_CODE(String m_CODE) {
-		M_CODE = m_CODE;
+	public void setM_code(String m_code) {
+		this.m_code = m_code;
 	}
 
-	public String getM_TITLE() {
-		return M_TITLE;
+	public String getM_title() {
+		return m_title;
 	}
 
-	public void setM_TITLE(String m_TITLE) {
-		M_TITLE = m_TITLE;
+	public void setM_title(String m_title) {
+		this.m_title = m_title;
 	}
 
-	public String getM_GENRE() {
-		return M_GENRE;
+	public String getM_genre() {
+		return m_genre;
 	}
 
-	public void setM_GENRE(String m_GENRE) {
-		M_GENRE = m_GENRE;
+	public void setM_genre(String m_genre) {
+		this.m_genre = m_genre;
 	}
 
-	public String getM_DIRECTOR() {
-		return M_DIRECTOR;
+	public String getM_director() {
+		return m_director;
 	}
 
-	public void setM_DIRECTOR(String m_DIRECTOR) {
-		M_DIRECTOR = m_DIRECTOR;
+	public void setM_director(String m_director) {
+		this.m_director = m_director;
 	}
 
-	public String getM_DATE() {
-		return M_DATE;
+	public String getM_date() {
+		return m_date;
 	}
 
-	public void setM_DATE(String m_DATE) {
-		M_DATE = m_DATE;
+	public void setM_date(String m_date) {
+		this.m_date = m_date;
 	}
 
-	public String getM_COUNTRY() {
-		return M_COUNTRY;
+	public String getM_country() {
+		return m_country;
 	}
 
-	public void setM_COUNTRY(String m_COUNTRY) {
-		M_COUNTRY = m_COUNTRY;
+	public void setM_country(String m_country) {
+		this.m_country = m_country;
 	}
 
-	public String getM_IMAGE() {
-		return M_IMAGE;
+	public String getM_image() {
+		return m_image;
 	}
 
-	public void setM_IMAGE(String m_IMAGE) {
-		M_IMAGE = m_IMAGE;
+	public void setM_image(String m_image) {
+		this.m_image = m_image;
 	}
 
-	public String getM_SUMMARY() {
-		return M_SUMMARY;
+	public String getM_summary() {
+		return m_summary;
 	}
 
-	public void setM_SUMMARY(String m_SUMMARY) {
-		M_SUMMARY = m_SUMMARY;
+	public void setM_summary(String m_summary) {
+		this.m_summary = m_summary;
 	}
 
-	public String getM_RATING() {
-		return M_RATING;
+	public String getM_rating() {
+		return m_rating;
 	}
 
-	public void setM_RATING(String m_RATING) {
-		M_RATING = m_RATING;
+	public void setM_rating(String m_rating) {
+		this.m_rating = m_rating;
 	}
 
-	public int getM_GRADE() {
-		return M_GRADE;
+	public int getM_grade() {
+		return m_grade;
 	}
 
-	public void setM_GRADE(int m_GRADE) {
-		M_GRADE = m_GRADE;
+	public void setM_grade(int m_grade) {
+		this.m_grade = m_grade;
 	}
 
-	public String getSTATUS() {
-		return STATUS;
+	public String getStatus() {
+		return status;
 	}
 
-	public void setSTATUS(String sTATUS) {
-		STATUS = sTATUS;
+	public void setStatus(String status) {
+		this.status = status;
 	}
 
 	@Override
 	public String toString() {
-		return "MovieVO [M_CODE=" + M_CODE + ", M_TITLE=" + M_TITLE + ", M_GENRE=" + M_GENRE + ", M_DIRECTOR="
-				+ M_DIRECTOR + ", M_DATE=" + M_DATE + ", M_COUNTRY=" + M_COUNTRY + ", M_IMAGE=" + M_IMAGE
-				+ ", M_SUMMARY=" + M_SUMMARY + ", M_RATING=" + M_RATING + ", M_GRADE=" + M_GRADE + ", STATUS=" + STATUS
+		return "MovieVO [m_code=" + m_code + ", m_title=" + m_title + ", m_genre=" + m_genre + ", m_director="
+				+ m_director + ", m_date=" + m_date + ", m_country=" + m_country + ", m_image=" + m_image
+				+ ", m_summary=" + m_summary + ", m_rating=" + m_rating + ", m_grade=" + m_grade + ", status=" + status
 				+ "]";
 	}
+	
+	
 
 }
